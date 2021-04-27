e8edc99a519796f4fdd63a8a88a88064293800b7
Marie Fedinová <marie.fedinova@gmail.com>
2021-04-27 20:14:27 +0200

    cvičení z hodiny

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ která vypíše kalorickou hodnotu potraviny.
 BONUS: počítej a zobrazuj celkový počet sněděných kalorií.
 Tj. při klikání na obrázky se postupně sčítají kalorie.
 */
+
+const obrazky = document.querySelectorAll('img');
+
+obrazky.forEach((obrazek) => {
+    obrazek.addEventListener("click", priKliknuti);
+})
+function priKliknuti(obrazek) {
+    let obrazekT = obrazek.target;
+    let kalorie = obrazekT.dataset.kalorie;
+    let vysledek = document.querySelector('#vysledek');
+    vysledek.innerHTML = kalorie;
+    let rodic = obrazekT.parentElement;
+    rodic.removeChild(obrazekT);
+    let snezeno = document.querySelector('#snezeno');
+    snezeno.appendChild(obrazekT);
+    obrazekT.removeEventListener("click", priKliknuti);
+}
